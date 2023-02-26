107c18721362ac821f742c1a884b20756c4d1a4f
mijan <jansson.michael.93@gmail.com>
2023-02-26 22:57:15 +0200

    almost prints how i want

--- main.go
+++ main.go
@@ -17,9 +17,12 @@ var (
 	reverse     bool   // -r flag
 	sortByTime  bool   // -t flag
 	recursive   bool   // -R flag
+	humanize    bool   // -h flag
 	dirPath     string // directory path to list
 )
 
+// Usage go run main.go and add the command line tex: "go run main.go -la -y"
+/*
 func main() {
 	for i := 1; i < len(os.Args); i++ {
 		switch os.Args[i] {
@@ -33,6 +36,8 @@ func main() {
 			sortByTime = true
 		case "-R":
 			recursive = true
+		case "-h":
+			humanize = true
 		case "-la":
 			showHidden = true
 			longListing = true
@@ -72,7 +77,11 @@ func main() {
 			}
 			total += info.Size()
 		}
-		fmt.Printf("total %d\n", total/1024)
+		if humanize {
+			fmt.Printf("total %s\n", humanizeBytes(total))
+		} else {
+			fmt.Printf("total %d\n", total/1024)
+		}
 		printLongListing(dirEntries)
 	} else {
 		printShortListing(dirEntries)
@@ -117,6 +126,367 @@ func main() {
 			log.Fatal(err)
 		}
 	}
+}*/
+/*
+func main() {
+	for i := 1; i < len(os.Args); i++ {
+		switch os.Args[i] {
+		case "-a":
+			showHidden = true
+		case "-l":
+			longListing = true
+		case "-r":
+			reverse = true
+		case "-t":
+			sortByTime = true
+		case "-R":
+			recursive = true
+		case "-h":
+			humanize = true
+		case "-la":
+			showHidden = true
+			longListing = true
+		default:
+			dirPath = os.Args[i]
+		}
+	}
+	if dirPath == "" {
+		dirPath = "."
+	}
+	dir, err := os.Open(dirPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dir.Close()
+	dirEntries, err := dir.ReadDir(0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if sortByTime {
+		sortByModificationTime(dirEntries)
+	} else if reverse {
+		sortReverse(dirEntries)
+	} else {
+		sortByName(dirEntries)
+	}
+
+	if longListing {
+		for _, entry := range dirEntries {
+			if !showHidden && entry.Name()[0] == '.' {
+				continue
+			}
+			info, err := entry.Info()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			totalSize += info.Size()
+		}
+		printLongListing(dirEntries)
+		if humanize {
+			fmt.Printf("total %s\n", humanizeBytes(totalSize))
+		} else {
+			fmt.Printf("total %d\n", totalSize/1024)
+		}
+	} else {
+		printShortListing(dirEntries)
+	}
+	if recursive {
+		err = filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				log.Printf("Error walking path %q: %v\n", path, err)
+				return nil
+			}
+			if info.IsDir() {
+				if info.Name() != "." && info.Name() != ".." {
+					fmt.Printf("\n%s:\n", path)
+					dir, err := os.Open(path)
+					if err != nil {
+						log.Printf("Error opening directory %q: %v\n", path, err)
+						return nil
+					}
+					defer dir.Close()
+					dirEntries, err := dir.ReadDir(0)
+					if err != nil {
+						log.Printf("Error reading directory %q: %v\n", path, err)
+						return nil
+					}
+					if sortByTime {
+						sortByModificationTime(dirEntries)
+					} else if reverse {
+						sortReverse(dirEntries)
+					} else {
+						sortByName(dirEntries)
+					}
+					if longListing {
+						printLongListing(dirEntries)
+					} else {
+						printShortListing(dirEntries)
+					}
+				}
+			}
+			return nil
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}*/
+/*
+func main() {
+	for i := 1; i < len(os.Args); i++ {
+		switch os.Args[i] {
+		case "-a":
+			showHidden = true
+		case "-l":
+			longListing = true
+		case "-r":
+			reverse = true
+		case "-t":
+			sortByTime = true
+		case "-R":
+			recursive = true
+		case "-h":
+			humanize = true
+		case "-la":
+			showHidden = true
+			longListing = true
+		default:
+			dirPath = os.Args[i]
+		}
+	}
+	if dirPath == "" {
+		dirPath = "."
+	}
+	dir, err := os.Open(dirPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dir.Close()
+	dirEntries, err := dir.ReadDir(0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if sortByTime {
+		sortByModificationTime(dirEntries)
+	} else if reverse {
+		sortReverse(dirEntries)
+	} else {
+		sortByName(dirEntries)
+	}
+
+	if longListing {
+		//var totalSize int64
+		for _, entry := range dirEntries {
+			if !showHidden && entry.Name()[0] == '.' {
+				continue
+			}
+			info, err := entry.Info()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			totalSize += info.Size()
+		}
+		if humanize {
+			fmt.Printf("total %s\n", humanizeBytes(totalSize))
+		} else {
+			fmt.Printf("total %d\n", totalSize/1024)
+		}
+		printLongListing(dirEntries)
+	} else {
+		printShortListing(dirEntries)
+	}
+
+	if recursive {
+		err = filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				log.Printf("Error walking path %q: %v\n", path, err)
+				return nil
+			}
+			if info.IsDir() {
+				if info.Name() != "." && info.Name() != ".." {
+					fmt.Printf("\n%s:\n", path)
+					dir, err := os.Open(path)
+					if err != nil {
+						log.Printf("Error opening directory %q: %v\n", path, err)
+						return nil
+					}
+					defer dir.Close()
+					dirEntries, err := dir.ReadDir(0)
+					if err != nil {
+						log.Printf("Error reading directory %q: %v\n", path, err)
+						return nil
+					}
+					if sortByTime {
+						sortByModificationTime(dirEntries)
+					} else if reverse {
+						sortReverse(dirEntries)
+					} else {
+						sortByName(dirEntries)
+					}
+					if longListing {
+						var totalSize int64
+						for _, entry := range dirEntries {
+							if !showHidden && entry.Name()[0] == '.' {
+								continue
+							}
+							info, err := entry.Info()
+							if err != nil {
+								log.Println(err)
+								continue
+							}
+							totalSize += info.Size()
+						}
+						if humanize {
+							fmt.Printf("total %s\n", humanizeBytes(totalSize))
+						} else {
+							fmt.Printf("total %d\n", totalSize/1024)
+						}
+						printLongListing(dirEntries)
+					} else {
+						printShortListing(dirEntries)
+					}
+				}
+			}
+			return nil
+		})*/
+
+func main() {
+	for i := 1; i < len(os.Args); i++ {
+		switch os.Args[i] {
+		case "-a":
+			showHidden = true
+		case "-l":
+			longListing = true
+		case "-r":
+			reverse = true
+		case "-t":
+			sortByTime = true
+		case "-R":
+			recursive = true
+		case "-h":
+			humanize = true
+		case "-la":
+			showHidden = true
+			longListing = true
+		default:
+			dirPath = os.Args[i]
+		}
+	}
+	if dirPath == "" {
+		dirPath = "."
+	}
+	dir, err := os.Open(dirPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dir.Close()
+	dirEntries, err := dir.ReadDir(0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if sortByTime {
+		sortByModificationTime(dirEntries)
+	} else if reverse {
+		sortReverse(dirEntries)
+	} else {
+		sortByName(dirEntries)
+	}
+
+	if longListing {
+		var totalSize int64
+		for _, entry := range dirEntries {
+			if !showHidden && entry.Name()[0] == '.' {
+				continue
+			}
+			info, err := entry.Info()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			totalSize += info.Size()
+		}
+		if humanize {
+			fmt.Printf("total %s\n", humanizeBytes(totalSize))
+		} else {
+			fmt.Printf("total %d\n", totalSize/1024)
+		}
+		printLongListing(dirEntries)
+	} else {
+		printShortListing(dirEntries)
+	}
+
+	if recursive {
+		err = filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				log.Printf("Error walking path %q: %v\n", path, err)
+				return nil
+			}
+			if info.IsDir() {
+				if info.Name() != "." && info.Name() != ".." {
+					fmt.Printf("\n%s:\n", path)
+					dir, err := os.Open(path)
+					if err != nil {
+						log.Printf("Error opening directory %q: %v\n", path, err)
+						return nil
+					}
+					defer dir.Close()
+					dirEntries, err := dir.ReadDir(0)
+					if err != nil {
+						log.Printf("Error reading directory %q: %v\n", path, err)
+						return nil
+					}
+					if sortByTime {
+						sortByModificationTime(dirEntries)
+					} else if reverse {
+						sortReverse(dirEntries)
+					} else {
+						sortByName(dirEntries)
+					}
+					if longListing {
+						var totalSize int64
+						for _, entry := range dirEntries {
+							if !showHidden && entry.Name()[0] == '.' {
+								continue
+							}
+							info, err := entry.Info()
+							if err != nil {
+								log.Println(err)
+								continue
+							}
+							totalSize += info.Size()
+						}
+						if humanize {
+							fmt.Printf("total %s\n", humanizeBytes(totalSize))
+						} else {
+							fmt.Printf("total %d\n", totalSize/1024)
+						}
+						printLongListing(dirEntries)
+					} else {
+						printShortListing(dirEntries)
+					}
+				}
+			}
+			return nil
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func humanizeBytes(b int64) string {
+	const unit = 1024
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB", float64(b)/float64(div), "KMGTPE"[exp])
 }
 
 func sortByName(entries []os.DirEntry) {
@@ -145,6 +515,7 @@ func sortReverse(entries []os.DirEntry) {
 	})
 }
 
+/*
 func printShortListing(entries []os.DirEntry) {
 	for _, entry := range entries {
 		if !showHidden && entry.Name()[0] == '.' {
@@ -156,8 +527,76 @@ func printShortListing(entries []os.DirEntry) {
 			fmt.Println(entry.Name())
 		}
 	}
+}*/
+
+func printShortListing(entries []os.DirEntry) {
+	for _, entry := range entries {
+		if !showHidden && entry.Name()[0] == '.' {
+			continue
+		}
+		if longListing {
+			mode := entry.Type().String()
+			info, err := entry.Info()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			fmt.Printf("%s %3d %s %s %6d %s %s\n",
+				mode, info.Sys().(*syscall.Stat_t).Nlink,
+				strconv.Itoa(int(info.Sys().(*syscall.Stat_t).Uid)),
+				strconv.Itoa(int(info.Sys().(*syscall.Stat_t).Gid)),
+				info.Size(), info.ModTime().Format("Jan _2 15:04"), entry.Name())
+
+		} else {
+			fmt.Println(entry.Name())
+		}
+	}
 }
 
+/*
+func printLongListing(entries []os.DirEntry) {
+	for _, entry := range entries {
+		if !showHidden && entry.Name()[0] == '.' {
+			continue
+		}
+		info, err := entry.Info()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		stat, ok := info.Sys().(*syscall.Stat_t)
+		if !ok {
+			log.Println("Failed to get syscall.Stat_t for entry:", entry.Name())
+			continue
+		}
+		mode := info.Mode().String()
+		size := info.Size()
+		uid := int(stat.Uid)
+		gid := int(stat.Gid)
+		userInfo, err := user.LookupId(strconv.Itoa(uid))
+		if err != nil {
+			log.Println("Failed to lookup user id:", uid, err)
+			continue
+		}
+		groupInfo, err := user.LookupGroupId(strconv.Itoa(gid))
+		if err != nil {
+			log.Println("Failed to lookup group id:", gid, err)
+			continue
+		}
+		timestamp := info.ModTime().Format("Jan _2 15:04")
+		if os.IsNotExist(err) {
+			fmt.Printf("%s %3d %s %s %6d %s %s\n", mode, info.Sys().(*syscall.Stat_t).Nlink, userInfo.Username, groupInfo.Name, size, timestamp, entry.Name())
+			continue
+		}
+		if err != nil {
+			log.Println("Failed to stat entry:", entry.Name(), err)
+			continue
+		}
+		fmt.Printf("%s %3d %s %s %6d %s %s\n", mode, stat.Nlink, userInfo.Username, groupInfo.Name, size, timestamp, entry.Name())
+	}
+}*/
+
+/*
 func printLongListing(entries []os.DirEntry) {
 	for _, entry := range entries {
 		if !showHidden && entry.Name()[0] == '.' {
@@ -191,4 +630,149 @@ func printLongListing(entries []os.DirEntry) {
 		timestamp := info.ModTime().Format("Jan _2 15:04")
 		fmt.Printf("%s %3d %s %s %6d %s %s\n", mode, info.Sys().(*syscall.Stat_t).Nlink, userInfo.Username, groupInfo.Name, size, timestamp, entry.Name())
 	}
+}
+*/
+/*
+func printLongListing(entries []os.DirEntry) {
+	var total int64
+	for _, entry := range entries {
+		if !showHidden && entry.Name()[0] == '.' {
+			continue
+		}
+		info, err := entry.Info()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		var stat syscall.Stat_t
+		err = syscall.Stat(entry.Name(), &stat)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		mode := info.Mode().String()
+		size := info.Size()
+		uid := int(stat.Uid)
+		gid := int(stat.Gid)
+		userInfo, err := user.LookupId(strconv.Itoa(uid))
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		groupInfo, err := user.LookupGroupId(strconv.Itoa(gid))
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		timestamp := info.ModTime().Format("Jan _2 15:04")
+		fmt.Printf("%s %3d %s %s %6d %s %s\n", mode, info.Sys().(*syscall.Stat_t).Nlink, userInfo.Username, groupInfo.Name, size, timestamp, entry.Name())
+		total += size
+	}
+	fmt.Printf("total %d\n", total/1024)
+}
+*/
+/*
+func printLongListing(entries []os.DirEntry) {
+	var total int64
+	for _, entry := range entries {
+		if !showHidden && entry.Name()[0] == '.' {
+			continue
+		}
+		info, err := entry.Info()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		var stat syscall.Stat_t
+		err = syscall.Stat(entry.Name(), &stat)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		mode := info.Mode().String()
+		size := info.Size()
+		total += size
+		uid := int(stat.Uid)
+		gid := int(stat.Gid)
+		userInfo, err := user.LookupId(strconv.Itoa(uid))
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		groupInfo, err := user.LookupGroupId(strconv.Itoa(gid))
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		timestamp := info.ModTime().Format("Jan _2 15:04")
+		fmt.Printf("%s %3d %s %s %6d %s %s\n", mode, info.Sys().(*syscall.Stat_t).Nlink, userInfo.Username, groupInfo.Name, size, timestamp, filepath.Base(entry.Name()))
+	}
+	fmt.Printf("total %d\n", total)
+}
+*/
+
+func printLongListing(entries []os.DirEntry) {
+	for _, entry := range entries {
+		if !showHidden && entry.Name()[0] == '.' {
+			continue
+		}
+		info, err := entry.Info()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		name := entry.Name()
+		if info.IsDir() {
+			name += "/"
+		}
+		stat, ok := info.Sys().(*syscall.Stat_t)
+		if !ok {
+			log.Println("Error getting system-specific file info")
+			continue
+		}
+		mode := info.Mode().String()
+		fmt.Printf("%s %3d %s %s %6d %s %s\n",
+			mode, stat.Nlink, getUserName(stat.Uid), getGroupName(stat.Gid),
+			info.Size(), info.ModTime().Format("Jan _2 15:04"), entry.Name())
+	}
+}
+
+/*
+func printLongListing(entries []os.DirEntry) {
+	for _, entry := range entries {
+		if !showHidden && entry.Name()[0] == '.' {
+			continue
+		}
+		info, err := entry.Info()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		perm := info.Mode().Perm().String()
+		size := humanizeBytes(info.Size())
+		modTime := info.ModTime().Format("Jan 02 15:04")
+		name := entry.Name()
+		if info.IsDir() {
+			name += "/"
+		}
+		fmt.Printf("%s %3d %-8s %-8d %-6s %s %s\n",
+			perm, info.Sys().(*syscall.Stat_t).Nlink, info.Sys().(*syscall.Stat_t).Uid, info.Sys().(*syscall.Stat_t).Gid,
+			size, modTime, name)
+	}
+}*/
+
+func getUserName(uid uint32) string {
+	user, err := user.LookupId(strconv.Itoa(int(uid)))
+	if err != nil {
+		return fmt.Sprintf("%d", uid)
+	}
+	return user.Username
+}
+
+func getGroupName(gid uint32) string {
+	group, err := user.LookupGroupId(strconv.Itoa(int(gid)))
+	if err != nil {
+		return fmt.Sprintf("%d", gid)
+	}
+	return group.Name
 }
