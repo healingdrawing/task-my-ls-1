d10d42de11b66697b7342311373b9600243fd265
mijan <jansson.michael.93@gmail.com>
2023-03-08 02:48:05 +0200

    empty

---
+++ lsColor/lsColor.go
@@ -0,0 +1,11 @@
+package lsColor
+
+/* colors and their meanings
+Blue: Directory /
+Green: Executable or recognized data file .
+Cyan (Sky Blue): Symbolic link file
+Yellow with black background: Device
+Magenta (Pink): Graphic image file
+Red: Archive file
+Red with black background: Broken link
+*/
